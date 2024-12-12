39b056321447ae0ef50b001e68705257b6c5a39e
Teguh Sujatmiko <tghsjtmk@gmail.com>
2024-12-12 10:04:51 +0700

    feat(new-guide) init nuxt website, init django rest api

--- app.config.ts
+++ app.config.ts
@@ -1,42 +1,42 @@
 export default defineAppConfig({
   docus: {
-    title: 'Teguh Sujatmiko',
-    description: 'The best place to start your documentation.',
-    image: 'https://karamel.id/_nuxt/img/logo-karamel.d68322c.png',
+    title: "Teguh Sujatmiko",
+    description: "The best place to start your documentation.",
+    image: "https://karamel.id/_nuxt/img/logo-karamel.d68322c.png",
     socials: {
-      instagram: 'teguh.sujatmiko',
-      twitter: 'tghsjtmk',
-      github: 'tghsjtmk',
+      instagram: "teguh.sujatmiko",
+      twitter: "tghsjtmk",
+      github: "tghsjtmk",
     },
-    layout: 'default',
+    layout: "default",
     aside: {
       level: 0,
-      exclude: []
+      exclude: [],
     },
     header: {
-      // fluid: true,
-      title: 'Teguh Sujatmiko',
+      fluid: true,
+      title: "Teguh Sujatmiko",
       showLinkIcon: true,
       exclude: [],
     },
     main: {
-      // fluid: true,
+      fluid: true,
       padded: true,
     },
     footer: {
-      // fluid: true,
+      fluid: true,
       credits: {
-        icon: 'IconDocus',
-        text: 'Powered by Docus',
-        href: 'https://docus.dev',
+        icon: "IconDocus",
+        text: "Powered by Docus",
+        href: "https://docus.dev",
       },
       textLinks: [
         {
-          text: 'Made with ❤️ Karamel.id',
-          href: 'https://karamel.id',
-          target: '_blank'
-        }
+          text: "Made with ❤️ Karamel.id",
+          href: "https://karamel.id",
+          target: "_blank",
+        },
       ],
-    }
-  }
-})
+    },
+  },
+});
